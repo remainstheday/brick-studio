9c8a7abd8167f702c96904315b981540b51b594b
remainstheday <trentontri@gmail.com>
2022-11-06 22:44:42 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "@livekit/react-components/dist/index.css";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import { PreJoinPage } from "./PreJoinPage";
+import { RoomPage } from "./RoomPage";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/room" element={<RoomPage />} />
+          <Route path="/" element={<PreJoinPage />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
